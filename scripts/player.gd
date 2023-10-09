9cce54ca31b9bffcd0a26235cf3c360fa081cc5d
Alejandro Corredor <acorredor@stoalabs.com>
2023-10-09 00:23:39 -0400

    start adding prone

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,11 +7,11 @@ const jump_velocity = 4.5
 const mouse_sensitivity = 0.25
 const lerp_speed = 10.0
 const free_look_tilt_amount = 8
-
-# Crouching
 const crouch_depth = 0.5
 const crouch_sprint_speed = 4.0
 const crouch_walk_speed = 2.0
+const prone_depth = 1.0
+const prone_speed = 1.0
 
 # Head bobbing
 const head_bobbing_walk_speed = 14.0
@@ -28,6 +28,7 @@ var current_speed = walk_speed
 var prev_speed = current_speed
 var direction = Vector3.ZERO
 var is_crouching = false
+var is_proning = false
 var is_jumping = false
 var is_free_looking = false
 var head_bobbing_vector = Vector2.ZERO
@@ -41,21 +42,35 @@ var last_velocity = Vector3.ZERO
 @onready var camera_3d = $neck/head/eyes/Camera3D
 @onready var standing_collision_shape = $standing_collision_shape
 @onready var crouching_collision_shape = $crouching_collision_shape
-@onready var ray_cast_3d = $RayCast3D
+@onready var prone_collision_shape = $prone_collision_shape
+@onready var standing_ray_cast = $standing_ray_cast
+@onready var crouching_ray_cast = $crouching_ray_cast
+@onready var proning_ray_cast = $proning_ray_cast
 @onready var animation_player = $neck/head/eyes/AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func handle_crouch(lerp_modifier: float): 
+func toggle_crouch(lerp_modifier: float): 
 	var crouch_height = lerp(head.position.y, -crouch_depth, lerp_modifier)
 	var standing_height = lerp(head.position.y, 0.0, lerp_modifier)
-	head.position.y = crouch_height  if is_crouching else standing_height
+	head.position.y = crouch_height if is_crouching else standing_height
 	standing_collision_shape.disabled = is_crouching
 	crouching_collision_shape.disabled = !is_crouching
 	
+func handle_prone(lerp_modifier: float): 
+	var prone_height = lerp(head.position.y, -prone_depth, lerp_modifier)
+	var standing_height = lerp(head.position.y, 0.0, lerp_modifier)
+	head.position.y = prone_height if is_proning else standing_height
+	standing_collision_shape.disabled = is_proning
+	prone_collision_shape.disabled = !is_proning
+	
 func handle_movement(delta: float, lerp_modifier: float, input_dir: Vector2):
-	if (Input.is_action_pressed("sprint")):
+	if (is_proning):
+		current_speed = prone_speed
+		head_bobbing_current_intensity = head_bobbing_crouch_walk_intensity if is_crouching else head_bobbing_walk_intensity
+		head_bobbing_index += head_bobbing_crouch_walk_speed * delta if is_crouching else head_bobbing_walk_speed * delta
+	elif (Input.is_action_pressed("sprint")):
 		if (is_on_floor()):
 			current_speed = crouch_sprint_speed if is_crouching else sprint_speed
 			head_bobbing_current_intensity = head_bobbing_crouch_sprint_intensity if is_crouching else head_bobbing_sprint_intensity
@@ -76,14 +91,13 @@ func handle_movement(delta: float, lerp_modifier: float, input_dir: Vector2):
 	else:
 		eyes.position.y = lerp(eyes.position.y, 0.0, lerp_modifier)
 		eyes.position.x = lerp(eyes.position.x, 0.0, lerp_modifier)
-	
-func handle_airtime():
+		
 	prev_speed = current_speed
-	
 	if (!is_on_floor()):
 		current_speed = current_speed - 2.0
 	else:
 		current_speed = prev_speed
+
 		
 func handle_free_look(lerp_modifier: float):
 	if (Input.is_action_pressed("free_look")):
@@ -114,11 +128,14 @@ func _input(event):
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 		
-	var blocked = is_crouching and ray_cast_3d.is_colliding()
-	if (Input.is_action_just_pressed("crouch") and !blocked and is_on_floor()):
+	if (Input.is_action_just_pressed("crouch") && !is_proning && !standing_ray_cast.is_colliding() && is_on_floor()):
 		is_crouching = !is_crouching
+	
+	if (Input.is_action_just_pressed("prone")):
+		var prone_blocked = proning_ray_cast.is_colliding();
+		is_proning = false if is_proning else !prone_blocked
 		
-	if (Input.is_action_just_pressed("ui_accept") and !ray_cast_3d.is_colliding() and is_on_floor()):
+	if (Input.is_action_just_pressed("ui_accept") && !standing_ray_cast.is_colliding() && is_on_floor()):
 		is_crouching = false
 		velocity.y = jump_velocity
 		animation_player.play("jump")
@@ -133,8 +150,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 	
 	handle_movement(delta, lerp_modifier, input_dir)	
-	handle_crouch(lerp_modifier)
-	handle_airtime()
+	toggle_crouch(lerp_modifier)
 	handle_free_look(lerp_modifier)
 	
 	# Get the input direction and handle the movement/deceleration.
