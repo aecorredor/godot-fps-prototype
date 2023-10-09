0a88dda2db79e4379c3b3f1d25e85b91c2439828
Alejandro Corredor <acorredor@stoalabs.com>
2023-10-08 21:52:34 -0400

    add head bobbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,46 +1,80 @@
 extends CharacterBody3D
 
-const crouch_depth = 0.5
-const walking_speed = 5.0
-const sprinting_speed = 8.5
-const crouch_sprinting_speed = 4.0
-const crouch_speed = 2.0
+# General Movement
+const walk_speed = 5.0
+const sprint_speed = 8.5
 const jump_velocity = 4.5
 const mouse_sensitivity = 0.25
 const lerp_speed = 10.0
 const free_look_tilt_amount = 8
-var current_speed = walking_speed
+
+# Crouching
+const crouch_depth = 0.5
+const crouch_sprint_speed = 4.0
+const crouch_walk_speed = 2.0
+
+# Head bobbing
+const head_bobbing_walk_speed = 14.0
+const head_bobbing_sprint_speed = 22.0
+const head_bobbing_crouch_walk_speed = 10.0
+const head_bobbing_crouch_sprint_speed = 12.0
+const head_bobbing_walk_intensity = 0.1
+const head_bobbing_sprint_intensity = 0.2
+const head_bobbing_crouch_walk_intensity = 0.05
+const head_bobbing_crouch_sprint_intensity = 0.075
+
+# Stateful
+var current_speed = walk_speed
 var prev_speed = current_speed
 var direction = Vector3.ZERO
 var is_crouching = false
 var is_jumping = false
 var is_free_looking = false
+var head_bobbing_vector = Vector2.ZERO
+var head_bobbing_index = 0.0
+var head_bobbing_current_intensity = 0.0
 
 @onready var neck = $neck
 @onready var head = $neck/head
+@onready var eyes = $neck/head/eyes
+@onready var camera_3d = $neck/head/eyes/Camera3D
 @onready var standing_collision_shape = $standing_collision_shape
 @onready var crouching_collision_shape = $crouching_collision_shape
 @onready var ray_cast_3d = $RayCast3D
-@onready var camera_3d = $neck/head/Camera3D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func handle_crouch(lerp_modifier: float): 
-	var crouched_height = lerp(head.position.y, -crouch_depth, lerp_modifier)
+	var crouch_height = lerp(head.position.y, -crouch_depth, lerp_modifier)
 	var standing_height = lerp(head.position.y, 0.0, lerp_modifier)
-	head.position.y = crouched_height  if is_crouching else standing_height
+	head.position.y = crouch_height  if is_crouching else standing_height
 	standing_collision_shape.disabled = is_crouching
 	crouching_collision_shape.disabled = !is_crouching
 	
-func handle_sprint():
+func handle_movement(delta: float, lerp_modifier: float, input_dir: Vector2):
 	if (Input.is_action_pressed("sprint")):
 		if (is_on_floor()):
-			current_speed = crouch_sprinting_speed if is_crouching else sprinting_speed
-		else:
+			current_speed = crouch_sprint_speed if is_crouching else sprint_speed
+			head_bobbing_current_intensity = head_bobbing_crouch_sprint_intensity if is_crouching else head_bobbing_sprint_intensity
+			head_bobbing_index += head_bobbing_crouch_sprint_speed * delta if is_crouching else head_bobbing_sprint_speed * delta
+		else: 
+			# Don't let the player start sprinting while in the air.
 			current_speed = prev_speed
 	else:
-		current_speed = crouch_speed if is_crouching else walking_speed	
+		current_speed = crouch_walk_speed if is_crouching else walk_speed	
+		head_bobbing_current_intensity = head_bobbing_crouch_walk_intensity if is_crouching else head_bobbing_walk_intensity
+		head_bobbing_index += head_bobbing_crouch_walk_speed * delta if is_crouching else head_bobbing_walk_speed * delta
 		
+	if (is_on_floor() && input_dir != Vector2.ZERO):
+		head_bobbing_vector.y = sin(head_bobbing_index)
+		head_bobbing_vector.x = sin(head_bobbing_index / 2) + 0.5
+		eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y * (head_bobbing_current_intensity / 2), lerp_modifier)
+		eyes.position.x = lerp(eyes.position.x, head_bobbing_vector.x * head_bobbing_current_intensity, lerp_modifier)
+	else:
+		eyes.position.y = lerp(eyes.position.y, 0.0, lerp_modifier)
+		eyes.position.x = lerp(eyes.position.x, 0.0, lerp_modifier)
+	
 func handle_airtime():
 	prev_speed = current_speed
 	
@@ -78,8 +112,8 @@ func _input(event):
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 		
-	var crouchingAndBlocked = is_crouching and ray_cast_3d.is_colliding()
-	if (Input.is_action_just_pressed("crouch") and !crouchingAndBlocked and is_on_floor()):
+	var blocked = is_crouching and ray_cast_3d.is_colliding()
+	if (Input.is_action_just_pressed("crouch") and !blocked and is_on_floor()):
 		is_crouching = !is_crouching
 		
 	if (Input.is_action_just_pressed("ui_accept") and !ray_cast_3d.is_colliding() and is_on_floor()):
@@ -88,19 +122,21 @@ func _input(event):
 
 		
 func _physics_process(delta):
-	handle_sprint()
 	var lerp_modifier = delta * lerp_speed
-	handle_crouch(lerp_modifier)
-	handle_airtime()
-	handle_free_look(lerp_modifier)
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
+	handle_movement(delta, lerp_modifier, input_dir)	
+	handle_crouch(lerp_modifier)
+	handle_airtime()
+	handle_free_look(lerp_modifier)
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	
 	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), lerp_modifier)
 	if direction:
 		velocity.x = direction.x * current_speed
